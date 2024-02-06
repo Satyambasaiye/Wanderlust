9eb32ddcbfc9ec18189c8d5e9edafdaa460b515b
Satyambasaiye <satyambasaiye123@gmail.com>
2024-02-06 23:25:47 +0530

    added packages

---
+++ src/main/java/com/app/validations/StartDateBeforeEnd.java
@@ -0,0 +1,20 @@
+package com.app.validations;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Retention(RUNTIME)
+@Target({TYPE})
+public @interface StartDateBeforeEnd {
+	String message() default "Start date must be before end date";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
