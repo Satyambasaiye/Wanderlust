b0ee875c79318451ec92361d1a21ea040b3b1a82
Amarsingh05 <pamar0522@gmail.com>
2024-02-19 13:10:41 +0530

    delete,update csd

--- src/main/java/com/app/controller/PackageController.java
+++ src/main/java/com/app/controller/PackageController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,6 +59,17 @@ public class PackageController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(packageService.addNewPackage(dto));
 		
 	}
+	@DeleteMapping("/{pacId}")
+	public ResponseEntity<?>deletePackage(@PathVariable String pacId){
+		System.out.println("in delete pac controller "+pacId);
+		return ResponseEntity.ok(packageService.deletePackage(pacId));
+	}
+	@PutMapping("/{pacId}")
+	public ResponseEntity<?>updatePackage(@PathVariable String pacId,@RequestBody @Valid PackageDto dto){
+		System.out.println("in update pac controller "+pacId+" "+dto);
+		return ResponseEntity.ok(packageService.updatePackage(pacId, dto));
+	}
+	
 	
 	
 	
